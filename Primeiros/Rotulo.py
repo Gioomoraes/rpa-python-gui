7444be14e1110014c34cfb75b0400a19ceb578dc
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2026-02-18 22:49:18 -0300

    test different relief styles

--- Primeiros/Rotulo.py
+++ Primeiros/Rotulo.py
@@ -3,8 +3,17 @@ from tkinter import *
 janela = Tk()
 janela.geometry("600x600")
 
-rotulo1 = Label(text="\npython", relief="flat", background="blue", foreground="white", font="Arial 40")
+rotulo1 = Label(text="python", relief="flat", background="blue", foreground="white", font="Arial 40")
 rotulo1.pack()
+
+rotulo2 = Label(text="python", relief="raised", background="purple", foreground="white", font="Arial 40")
+rotulo2.pack()
+
+rotulo3 = Label(text="python", relief="sunken", background="green", foreground="white", font="Arial 40")
+rotulo3.pack()
+
+rotulo4 = Label(text="python", relief="groove", background="pink", foreground="white", font="Arial 40")
+rotulo4.pack()
 
 
 janela.title("interface")
