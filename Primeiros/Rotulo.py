c2d28d78c09d1af4193260399f1d7d56397cfeee
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2026-02-18 22:33:40 -0300

    add styled Tkinter label component

--- Primeiros/Rotulo.py
+++ Primeiros/Rotulo.py
@@ -3,11 +3,9 @@ from tkinter import *
 janela = Tk()
 janela.geometry("600x600")
 
-instrucao = Label(text="\nBem-vindo", font="Arial 40")
-instrucao.pack()
+rotulo1 = Label(text="\npython", relief="flat", background="blue", foreground="white", font="Arial 40")
+rotulo1.pack()
 
-instrucao_2 = Label(text="\nTestanto Interface", font="Arial 40")
-instrucao_2.pack()
 
 janela.title("interface")
 
