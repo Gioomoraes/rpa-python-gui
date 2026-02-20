29b20f93d0a458dfb39d010e673dd9d87562c071
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2026-02-19 22:15:16 -0300

    add multiline label example

--- Primeiros/Rotulo.py
+++ Primeiros/Rotulo.py
@@ -3,17 +3,28 @@ from tkinter import *
 janela = Tk()
 janela.geometry("600x600")
 
-rotulo1 = Label(text="python", relief="flat", background="blue", foreground="white", font="Arial 40")
+rotulo1 = Label(janela, text="python", relief="flat", background="blue", foreground="white", font="Arial 40")
 rotulo1.pack()
 
-rotulo2 = Label(text="python", relief="raised", background="purple", foreground="white", font="Arial 40")
+rotulo2 = Label(janela, text="python", relief="raised", background="purple", foreground="white", font="Arial 40")
 rotulo2.pack()
 
-rotulo3 = Label(text="python", relief="sunken", background="green", foreground="white", font="Arial 40")
+rotulo3 = Label(janela, text="python", relief="sunken", background="green", foreground="white", font="Arial 40")
 rotulo3.pack()
 
-rotulo4 = Label(text="python", relief="groove", background="pink", foreground="white", font="Arial 40")
+rotulo4 = Label(janela, text="python", relief="groove", background="pink", foreground="white", font="Arial 40")
 rotulo4.pack()
+
+texto = """ Testando Tkinter
+Testando interface
+com python
+"""
+
+rotulo5 = Label(janela,
+                font="arial 40 bold",
+                text= texto)
+
+rotulo5.pack()
 
 
 janela.title("interface")
