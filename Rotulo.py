dc2e758000a54b7be05cdf7cb23f4622c85bfed7
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2026-02-20 15:08:32 -0300

    ignore pyinstaller build files

---
+++ Rotulo.py
@@ -0,0 +1,33 @@
+from tkinter import *
+
+janela = Tk()
+janela.geometry("850x850")
+
+rotulo1 = Label(janela, text="python", relief="flat", background="blue", foreground="white", font="Arial 40")
+rotulo1.pack()
+
+rotulo2 = Label(janela, text="python", relief="raised", background="purple", foreground="white", font="Arial 40")
+rotulo2.pack()
+
+rotulo3 = Label(janela, text="python", relief="sunken", background="green", foreground="white", font="Arial 40")
+rotulo3.pack()
+
+rotulo4 = Label(janela, text="python", relief="groove", background="pink", foreground="white", font="Arial 40")
+rotulo4.pack()
+
+texto = """ Testando Tkinter
+Testando interface
+com python
+"""
+
+rotulo5 = Label(janela,
+                font="arial 40 bold",
+                text= texto)
+
+rotulo5.pack()
+
+
+janela.title("interface")
+
+
+janela.mainloop()
