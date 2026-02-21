4073aff03b61d210901ae52bcab437c6816e6329
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2026-02-21 18:35:25 -0300

    add pillow image loading

--- Primeiros/image.py
+++ Primeiros/image.py
@@ -1,10 +1,14 @@
 from tkinter import *
+from PIL import ImageTk, Image
 
 janela = Tk()
 janela.geometry("600x600")
+janela.title("image")
 
+Label(janela, text="image", font="Arial 20").pack(side="top")
 
-janela.title("interface")
+CaminhoImagem = ImageTk.PhotoImage(Image.open(""))
 
+botaoImagem = Button(image=CaminhoImagem, command=janela.destroy).pack(side="left")
 
 janela.mainloop()
