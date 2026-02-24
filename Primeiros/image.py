552f60b0adfd26e149bf3dbec7a75f07add3aba0
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2026-02-23 22:12:42 -0300

    add background image to tkinter window

--- Primeiros/image.py
+++ Primeiros/image.py
@@ -5,9 +5,16 @@ janela = Tk()
 janela.geometry("600x600")
 janela.title("image")
 
+CaminhoImagemFundo = Image.open("C:\\Users\\giovanna\\projetos\\python\\Primeiros\\Fundo.jpg")
+img = ImageTk.PhotoImage(CaminhoImagemFundo)
+labelFundo = Label(janela, image=img)
+
+labelFundo.place(x=0, y=0)
+
+
 Label(janela, text="image", font="Arial 20").pack(side="top")
 
-CaminhoImagem = ImageTk.PhotoImage(Image.open(""))
+CaminhoImagem = ImageTk.PhotoImage(Image.open("SAIR.jpg"))
 
 botaoImagem = Button(image=CaminhoImagem, command=janela.destroy).pack(side="left")
 
