65395a0edb567e58aa6d4f7525713e4ff8db4020
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2026-02-26 22:22:00 -0300

    add name and username entry fields

--- Primeiros/Entry.py
+++ Primeiros/Entry.py
@@ -7,5 +7,19 @@ janela.geometry("600x600")
 
 janela.title("Entry")
 
+nome = Label(text="Nome: ",
+             font="Arial 40")
+
+nome.grid(row=1, column=0, sticky="W")
+
+exibirNome = Entry(font= "Arial 40")
+exibirNome.grid(row=1, column=1, sticky="W")
+
+sobrenome = Label(text="Sobrenome: ",
+             font="Arial 40")
+
+sobrenome.grid(row=2, column=0, sticky="W")
+exibirSobrenome = Entry(font="Arial 40")
+exibirSobrenome.grid(row=2, column=1, sticky="W")
 
 janela.mainloop()
