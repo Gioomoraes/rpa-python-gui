c1d2e3fda4dac85a5b862bad2ff7c5451c14948d
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2026-04-24 19:25:22 -0300

    Adiciona Checkbutton com valor numérico e início da lógica de soma

--- Primeiros/CheckButtonNumbers.py
+++ Primeiros/CheckButtonNumbers.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 janela = Tk()
 janela.geometry("600x600")
@@ -6,5 +7,24 @@ janela.geometry("600x600")
 
 janela.title("CheckButton 2")
 
+labelInformation = Label(janela, text="selecione a opção",
+                         foreground="blue",
+                         font=("Arial", 25)).pack()
+
+total = 0
+valorAntigo = 0
+
+def funcaoSomar():
+
+    global total
+
+varNumber = IntVar()
+
+checkNumber5 = Checkbutton(janela, text="5",
+                        variable=varNumber,
+                        font=("Arial", 25),
+                        onvalue=5,
+                        offvalue= 0,
+                        command=funcaoSomar).pack()
 
 janela.mainloop()
