919d3ad7a288b843bb42d76b945bb18ec1a11731
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2026-05-15 16:30:16 -0300

    implement basic config

--- Primeiros/RadionButton.py
+++ Primeiros/RadionButton.py
@@ -5,4 +5,11 @@ janela.title("RadioButton")
 
 janela.geometry("400x300")
 
+variavelOS = StringVar(janela, "0")
+
+radiobutton_1 = Radiobutton(janela,
+                            text="Letra A",
+                            font=("Arial", 20),
+                            variable="variavelOS")
+
 janela.mainloop()
