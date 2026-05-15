85be278f18e966ab16c8a18bc2e1d99b3239c5fb
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2026-05-15 16:32:58 -0300

    print selected value

--- Primeiros/RadionButton.py
+++ Primeiros/RadionButton.py
@@ -5,11 +5,16 @@ janela.title("RadioButton")
 
 janela.geometry("400x300")
 
+def imprimirItemSelecionado():
+
+    print("Você selecionou a letra: " + variavelOS.get())
+
 variavelOS = StringVar(janela, "0")
 
 radiobutton_1 = Radiobutton(janela,
                             text="Letra A",
                             font=("Arial", 20),
-                            variable="variavelOS")
-
+                            value="A",
+                            variable="variavelOS",
+                            command=imprimirItemSelecionado)
 janela.mainloop()
