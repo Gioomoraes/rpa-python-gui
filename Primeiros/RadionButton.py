d27255591d889f27c048874b1e8f8a78856add76
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2026-05-19 15:20:49 -0300

    salvando mudanças locais

--- Primeiros/RadionButton.py
+++ Primeiros/RadionButton.py
@@ -7,14 +7,14 @@ janela.geometry("400x300")
 
 def imprimirItemSelecionado():
 
-    print("Você selecionou a letra: " + variavelOS.get())
+    print("Você selecionou a letra: " + variavelOS.get() )
 
-variavelOS = StringVar(janela, "0")
+variavelOS = StringVar(janela,"0")
 
 radiobutton_1 = Radiobutton(janela,
                             text="Letra A",
                             font=("Arial", 20),
                             value="A",
-                            variable="variavelOS",
-                            command=imprimirItemSelecionado)
+                            variable=variavelOS,
+                            command=imprimirItemSelecionado).pack()
 janela.mainloop()
